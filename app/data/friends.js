bb0862cc9e3fa3c55caf897d4a19d5ba0c73bf31
Jay Yang <hjyang1@uci.edu>
2017-12-04 15:19:49 -0800

    final touches, touch up html

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,56 @@ var friendsList = [
             4,
             1
         ]
-    }
+    },
+
+    {
+        name: "SpongeBob",
+        photo: "https://vignette.wikia.nocookie.net/spongebob/images/a/ac/Spongebobwithglasses.jpeg/revision/latest?cb=20121014113150",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Patrick",
+        photo: "https://static.esea.net/global/images/users/1559972.1486072809.png",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Squidward",
+        photo: "https://pbs.twimg.com/media/DN-SDIXU8AAkdaK.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
 
 ]
 
