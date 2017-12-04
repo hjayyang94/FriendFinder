3c70d5b6ad4dd62b32ee9dac2d9617693329bb6a
Jay Yang <hjyang1@uci.edu>
2017-12-04 15:11:10 -0800

    slight edits with html grammar, added face to Ahmed test case

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsList = [
     {
         name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://facegen.com/images/rand_face/01.jpg",
         scores: [
             5,
             1,
